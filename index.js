b51c662308817f446389857b8847994e30f0c780
Devsingh0123 <dayamns20@gmail.com>
2026-02-08 16:48:10 +0530

    join, toString and toLocalString

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 
-const numsArray=[1,0,2,302,34,4,0];
+const numsArray=[1,0,2,302000,34,4,0];
 const stringArray = ["this", "is", "what" , "i", "want"];
 
 
+// methods for converting an array into string
+
+// 1.join
+// let newArray = numsArray.join(" ") 
+// console.log(newArray)
+
+// console.log(typeof(newArray))
 
 
-// 1.indexOf
-console.log(numsArray.indexOf(0));//returns first element's index of an array
-console.log(stringArray.indexOf("Is"));//case sensitive
-// if element is not found then return -1
+
+// 2.toString
+// let newArray = numsArray.toString()
+// console.log(newArray)
+
+// console.log(typeof(newArray))
 
 
 
-// 2. includes
-console.log(numsArray.includes(0))//if element is present in an array then return true otherwise false
-console.log(stringArray.includes("Is"))//also case sensitive
+// 2.toString
+let newArray = numsArray.toLocaleString() //ye toString ki tarah hi hai bas ye readiblity inhance karta hai 
+console.log(newArray)
 
-//3. lastIndexOf
-console.log(numsArray.lastIndexOf(0)) // if there is more than 1 same element found in an array then it is returns the last element's index of an arrray
-// if elemaent is not found in an array then returns -1
+console.log(typeof(newArray))
