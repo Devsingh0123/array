fd4fad0c2d335d58181aeb8b90f27042b3105091
Devsingh0123 <dayamns20@gmail.com>
2026-03-01 22:52:00 +0530

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const stringArray = ["this","is", "what" , "i", "want"];
 
 
 // let map = numsArray.map(nums=>nums*2) //ha jo bhi operation karenge uss aaadhar par wo hame 1 new array return kar ke dega
-// console.log(map)
+// console.log(map) 
 
 // let filter = numsArray.filter(nums=>nums>2) //condition ke aadhar par hame result ke rup me 1 array return ker ke dega jo v hamare condition ko satisfy karega
 // console.log(filter)
